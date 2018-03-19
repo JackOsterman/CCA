9cb282c6d10e42aaaec2e570917234d088cac7d4
JackOsterman <josterman3@gmail.com>
2018-03-19 09:41:10 -0400

    created standalone with PyInstaller
    make sure to do this before trying to compile standalone https://stackoverflow.com/questions/47318119/no-module-named-pandas-libs-tslibs-timedeltas-in-pyinstaller

--- automation.py
+++ automation.py
@@ -22,7 +22,7 @@ def getColor(name):
         if wb.iloc[k]['School'] == name:
             color1 = wb.iloc[k]['Color 1']
             color2 = wb.iloc[k]['Color 2']
-    if color1 == '':    
+    if color1 == '':
         print('School not found\nEnter hex codes of school (# included)')
         color1 = input('Color 1: ')
         color2 = input('Color 2: ')
@@ -37,20 +37,20 @@ def getRoster(name):
 
     roster = ['']*5
     n = 0
-    
+
     for k in range(0,num):
         if wb.iloc[k]['Team Name'] == name:
             roster[n] = wb.iloc[k]['Username']
             n = n + 1
     return(roster)
-            
+
 def setRoster(side, roster):
 
     for k in range(1,6):
         p = open('%sPlayer%d.txt' % (side, k),'w')
         p.write(roster[k-1])
         p.close
-    
+
 
 def writeHTML(side,color1,color2):
     s = ['<html>\n<head>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n<style>\n#secondary-color {\n  height: 100px;\n  width: 100px;\n  overflow: hidden;\n  background-color: ',
@@ -68,28 +68,28 @@ def writeHTML(side,color1,color2):
 def hexToRGB(color):
     r,g,b = webcolors.hex_to_rgb(color)
     return(r,g,b)
-        
-    
+
+
 
 def newMatch():
 
     t1 = team()
     t2 = team()
     t1.side, t2.side = 'blue','orange'
-    
+
     t1.school = input('Enter Home (blue team) School Name: ')
     t1.color1, t1.color2 = getColor(t1.school)
     t2.school = input('Enter Away (orange team) School Name: ')
     t2.color1, t2.color2 = getColor(t2.school)
-    
+
     print("Home team: {0}\n\tHex Colors: {1}, {2}\n\tRGB colors: {3}, {4}" .format(t1.school,t1.color1,t1.color2,hexToRGB(t1.color1),hexToRGB(t1.color2)))
     print("Away team: {0}\n\tHex Colors: {1}, {2}\n\tRGB colors: {3}, {4}" .format(t2.school,t2.color1,t2.color2,hexToRGB(t2.color1),hexToRGB(t2.color2)))
-    
+
     writeHTML(t1.side,t1.color1,t1.color2)
     writeHTML(t2.side,t2.color1,t2.color2)
 
     print('\nPress ENTER to exit')
-    
+
 
 newMatch()
 input()
