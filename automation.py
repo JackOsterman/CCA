251ff405cc4746e848e746856d079bf7d3c30942
JackOsterman <josterman3@gmail.com>
2018-03-19 16:38:02 -0400

    Implemented roster functions
    grabs players from 'roster test.xlsx', uses columns named 'Username','Team name', and 'College'. Column names may change depending on AVGL.
    
    Implemented for blue team only

--- automation.py
+++ automation.py
@@ -5,7 +5,8 @@ from PIL import Image, ImageDraw
 
 
 class team(object):
-    name, school, side, color1, color2, alt1, alt2, player1, player2, player3, player4, player5 = ['']*12
+    name, school, scoreboard, side, color1, color2, alt1, alt2 = ['']*8
+    roster = ['']*5
     init = False
     def __init__(self):
         init = True
@@ -32,7 +33,7 @@ def getColor(name):
     return(color1, color2)
 
 def getRoster(name):
-    wb = pd.read_excel('Team Colors.xlsx')
+    wb = pd.read_excel('roster test.xlsx')
 
     num = len(wb.index)
 
@@ -40,10 +41,45 @@ def getRoster(name):
     n = 0
 
     for k in range(0,num):
-        if wb.iloc[k]['Team Name'] == name:
+        if wb.iloc[k]['Team name'] == name:
             roster[n] = wb.iloc[k]['Username']
             n = n + 1
     return(roster)
+
+def getSchool(name):
+    wb = pd.read_excel('roster test.xlsx')
+
+    num = len(wb.index)
+
+    school = ''
+
+    for k in range(0,num):
+        if wb.iloc[k]['Team name'] == name:
+            school = wb.iloc[k]['College']
+
+    return(school)
+
+def printRoster(name, roster):
+    print('{0} roster:\n{1}'.format(name,roster))
+    for k in range(0,5):
+        print('[{0}] {1}'.format(k,roster[k]))
+
+def changePlayer(roster):
+    playerNum = int(input('Enter number of player you wish to edit: '))
+    roster[playerNum] = input('Enter new name for {0}: '.format(roster[playerNum]))
+
+    return(roster)
+
+
+def changeRoster(name,roster):
+    choice = 'y'
+    while choice == 'y':
+        printRoster(name,roster)
+        choice = input('Change player name? (y/n)')
+        if choice == 'y':
+            roster = changePlayer(roster)
+    return(roster)
+
 
 def setRoster(side, roster):
 
@@ -51,7 +87,6 @@ def setRoster(side, roster):
         p = open('%sPlayer%d.txt' % (side, k),'w')
         p.write(roster[k-1])
         p.close
-
 
 def createHTML(side,color1,color2):
     s = ['<html>\n<head>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n<style>\n#secondary-color {\n  height: 100px;\n  width: 100px;\n  overflow: hidden;\n  background-color: ',
@@ -77,21 +112,29 @@ def hexToRGB(color):
     r,g,b = webcolors.hex_to_rgb(color)
     return(r,g,b)
 
-
-
 def newMatch():
 
     t1 = team()
     t2 = team()
     t1.side, t2.side = 'blue','orange'
 
-    t1.school = input('Enter Home (blue team) School Name: ')
+    t1.name = input('Enter Home (blue team) Team Name: ')
+    t1.school = getSchool(t1.name)
     t1.color1, t1.color2 = getColor(t1.school)
-    t2.school = input('Enter Away (orange team) School Name: ')
+
+    t2.name = input('Enter Away (orange team) Team Name: ')
+    t2.school = getSchool(t2.name)
     t2.color1, t2.color2 = getColor(t2.school)
 
     print("Home team: {0}\n\tHex Colors: {1}, {2}\n\tRGB colors: {3}, {4}" .format(t1.school,t1.color1,t1.color2,hexToRGB(t1.color1),hexToRGB(t1.color2)))
     print("Away team: {0}\n\tHex Colors: {1}, {2}\n\tRGB colors: {3}, {4}\n" .format(t2.school,t2.color1,t2.color2,hexToRGB(t2.color1),hexToRGB(t2.color2)))
+
+    t1.roster = getRoster(t1.name)
+
+    t1.roster = changeRoster(t1.name,t1.roster)
+
+    setRoster(t1.side,t1.roster)
+
 
 
     print('Generating HTML Files...')
---
+++ run_pyinstaller.bat
@@ -0,0 +1,1 @@
+pyinstaller automation.py --onefile
